59c8fb6f4e4c3733ac5c73dbd0b41240b11dfab8
arm avi <armavicse@gmail.com>
2021-08-13 14:06:02 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CMED Task-1
+ CMED Task-1 is Android application assessment test from CMED Health
---
+++ app/src/main/java/com/cmed/task1/services/RetrofitInterface.java
@@ -0,0 +1,12 @@
+package com.cmed.task1.services;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+
+public interface RetrofitInterface {
+    @GET("intrvw/SampleVideo_1280x720_30mb.mp4")
+    @Streaming
+    Call<ResponseBody> downloadFile();
+}
